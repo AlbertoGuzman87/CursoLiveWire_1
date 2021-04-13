1741ec6f0c361eaa0f9f95c18ad77c91444cc0c8
Juan Alberto <jguzman.pearbit@gmail.com>
2021-04-12 19:41:54 -0500

    Crear post video 08

--- app/Http/Livewire/CreatePost.php
+++ app/Http/Livewire/CreatePost.php
@@ -7,12 +7,25 @@ use Livewire\Component;
 
 class CreatePost extends Component
 {
-    public $open = false;
+    public $open = true;
 
     public $titulo, $contenido;
 
+    protected $rules = [
+        'titulo' => 'required',
+        'contenido' => 'required',
+    ];
+
+    //Método que rendiriza en tiempo real
+    // public function updated($nombrePropiedad)
+    // {
+    //     $this->validateOnly($nombrePropiedad);
+    // }
+
     public function save()
     {
+        $this->validate();
+
         Post::create([
             'titulo' => $this->titulo,
             'contenido' => $this->contenido,
@@ -20,14 +33,13 @@ class CreatePost extends Component
 
 
         //Cierra la alerta y resetea los inputs
-        $this->reset(['open','titulo','contenido']);
+        $this->reset(['open', 'titulo', 'contenido']);
         //Evento que hace refrescar la tabla
         // $this->emit('refresca');
         //Evento que hace refrescar la tabla unicamente al controlador especifico
-        $this->emitTo('show-post','refresca');
+        $this->emitTo('show-post', 'refresca');
 
         $this->emit('alert', 'El post se genero satisfactoriamente');
-
     }
     public function render()
     {
--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -14,14 +14,20 @@
                 <x-jet-label value="Título del Post" />
                 {{-- wire:model.defer
                 Ayuda a detener la comunicación de controlador y refrescar --}}
-                <x-jet-input wire:model.defer="titulo" type="text" class="w-full" />
+                <x-jet-input wire:model="titulo" type="text" class="w-full" />
+                @error('titulo')
+                    <x-jet-input-error for="titulo" />
+                @enderror
             </div>
 
             <div class="mb-4">
                 <x-jet-label value="Contenido del Post" />
                 {{-- wire:model.defer
                     Ayuda a detener la comunicación de controlador y refrescar --}}
-                <textarea wire:model.defer="contenido" name="" class="form-control w-full" rows="6"></textarea>
+                <textarea wire:model="contenido" name="" class="form-control w-full" rows="6"></textarea>
+                @error('contenido')
+                    <x-jet-input-error for="contenido" />
+                @enderror
             </div>
         </x-slot>
 
@@ -29,11 +35,15 @@
             <x-jet-secondary-button wire:click="$set('open',false)">
                 Cancelar
             </x-jet-secondary-button>
-            <x-jet-danger-button wire:click="save">
+            <x-jet-danger-button wire:click="save" wire:loading.attr="disabled" wire:target="save"
+                class="disabled:opacity-25">
+                {{-- Boton que se oculta y muestra el texto de abajo --}}
+                {{-- <x-jet-danger-button wire:click="save" wire:loading.remove wire:target="save" class="disable:opacity-25"> --}}
                 Crear Post
             </x-jet-danger-button>
+            {{-- Texto que se muestra al dar click al boton --}}
+            {{-- <span wire:loading wire:target="save">Cargando...</span> --}}
         </x-slot>
 
     </x-jet-dialog-modal>
-
 </div>
